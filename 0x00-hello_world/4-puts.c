bf9b0985be050e5602ed5a7b878a0ca5ffc5ac90
Nwankz <koolemi@gmail.com>
2023-08-10 07:12:43 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main A Program that prints a line with puts funtion
+ * Return: o (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (o);
+}
