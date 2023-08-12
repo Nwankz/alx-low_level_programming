c4fb955bfdfc6a3a9eefdaac4e61fe9e0e59199e
Nwankz <koolemi@gmail.com>
2023-08-11 20:07:02 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char C;
+
+	C = 'a';
+	while
+		(C <= 'Z') {
+			putchar(C);
+			C++;
+		}
+	putchar('\n');
+	return (0);
+}
